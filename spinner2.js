b57c8586b0a616e600fcbf1acf0d5b24ceb982ee
Bruce Lee <brucehlee@yahoo.ca>
2023-02-17 10:57:29 -0800

    Updated spinner2.js

---
+++ spinner2.js
@@ -0,0 +1,12 @@
+
+
+let array = ['|', '/', '-' , '\\' , '|' , '/', '-','\n'];
+let delay = 100;
+
+for (let i = 0; i < array.length; i++) {
+  delay += 200;
+  setTimeout(() => {
+    process.stdout.write(`\r${array[i]} `);
+  } , delay);
+}
+
